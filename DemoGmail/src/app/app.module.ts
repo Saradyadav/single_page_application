a42f25921a6956cc9806498bef0d1e921118ab0e
saradyadav <sarad.ky@samsung.com>
2023-07-10 09:29:25 +0530

    created another component named "PostComponent"

--- DemoGmail/src/app/app.module.ts
+++ DemoGmail/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PostCompnentComponent } from './post-compnent/post-compnent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PostCompnentComponent
   ],
   imports: [
     BrowserModule,
---
+++ DemoGmail/src/app/post-compnent/post-compnent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-compnent',
+  templateUrl: './post-compnent.component.html',
+  styleUrls: ['./post-compnent.component.css']
+})
+export class PostCompnentComponent {
+
+}
